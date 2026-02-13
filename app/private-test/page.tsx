166b50d6893bbac70b27b3266909c535f0092bdb
Mustafa Said Bektaş <145277969+mustfsb@users.noreply.github.com>
2026-02-13 03:31:12 +0300

    Update testId value in PrivateTestPage

--- app/private-test/page.tsx
+++ app/private-test/page.tsx
@@ -28,7 +28,7 @@ interface QuestionData {
 }
 
 export default function PrivateTestPage() {
-  const testId = "1062085";
+  const testId = "1066619";
   
   const [mounted, setMounted] = useState(false);
   const [loading, setLoading] = useState(true);
